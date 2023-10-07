c3b49afb15ad3493395ddcc841c577569067572f
Youssef Addouli Fernandez <yuu@youssefs-macbook-pro.home>
2023-10-07 21:21:10 +0200

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...values) {
+  return list.filter((listItem) => !values.includes(listItem));
 };
 
 // Do not edit below this line
