e4a1f1babd28c3084835b24dd6c292dea3df7958
Youssef Addouli Fernandez <yuu@youssefs-macbook-pro.home>
2023-10-07 21:40:29 +0200

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let result = 0;
 
+  //Test for values being numbers
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    //Test for numbers being postivive
+    if (num1 > 0 && num2 > 0) {
+      //We sort the numbers
+      let auxArray = [num1, num2].sort();
+      num1 = auxArray[0];
+      num2 = auxArray[1];
+
+      for (let i = num1; i <= num2; i++) {
+        result += i;
+      }
+
+    } else {
+      result = 'ERROR';
+    }
+  } else {
+    result = 'ERROR';
+  }
+
+  return result;
 };
 
 // Do not edit below this line
